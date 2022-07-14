3bd4671ff732110f6f4e254487a2cf3d7df1d4ae
Anca Alexia Nistor <91622899+915-Nistor-Anca@users.noreply.github.com>
2022-07-14 18:06:37 +0300

    Delete human.py

--- human.py
+++
@@ -1,7 +0,0 @@
-from board.cell import Cell
-from player.player import Player
-
-
-class Human(Player):
-    def move(self, line, column, value):
-        self._board_shots.set_value(line, column, value)
