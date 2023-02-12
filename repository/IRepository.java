1c551fcc1d9323e580776db0fb07bbd3c8542c2f
Anca Alexia Nistor <91622899+915-Nistor-Anca@users.noreply.github.com>
2023-02-12 14:13:15 +0200

    Delete repository directory

--- repository/IRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.IVegetable;
-
-public interface IRepository {
-    public void add(IVegetable vegetable);
-    public void delete(int index);
-    public IVegetable[] getAll();
-}
