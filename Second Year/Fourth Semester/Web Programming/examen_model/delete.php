17301318979ed9a70b7c89f0221aedb9e286582d
Anca Alexia Nistor <91622899+915-Nistor-Anca@users.noreply.github.com>
2023-06-20 15:28:47 +0300

    Add files via upload

---
+++ Second Year/Fourth Semester/Web Programming/examen_model/delete.php
@@ -0,0 +1,56 @@
+<?php 
+  session_start();
+
+  if ($_SESSION['logged_in'] == 'false') {
+    $_SESSION['login'] = 'Log in';
+    $_SESSION['message'] = '';
+    header("location: index.php");
+  }
+
+  if ($_SESSION['selected_project_id'] == '') {
+    header("location: index.php");
+  }
+
+  $mysqli = new mysqli('localhost', 'root', '', 'examen_model');
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $id = $_SESSION['selected_project_id'];
+    $sql = "DELETE FROM project WHERE id = '$id'";
+
+    if ($mysqli->query($sql) === true) {
+      $_SESSION['selected_project_id'] = '';
+      header("location: index.php");
+    } else {
+      header("location: delete.php");
+    }
+
+  }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Projects</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+  <div class="login-page">
+    <div class="form">
+
+      <form class="register-form" method="POST" action="delete.php">
+        <input class="submit" type="submit" name="delete" value="Delete project">
+
+      </form>
+
+    </div>
+  </div>
+
+</body>
+
+</html>
